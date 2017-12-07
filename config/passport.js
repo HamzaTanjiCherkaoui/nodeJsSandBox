cef79d2bf386324dd43987a72260075171073073
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-07 12:19:51 +0000

    WIP

--- config/passport.js
+++ config/passport.js
@@ -11,11 +11,11 @@ passport.use(
         },
         function (email, password, done) {
             User.findOne({ email: email }).then(function (user) {
-                if(!user || !user.validPassword(password){
+                if(!user || !user.validPassword(password)){
                     return done(null,false , { errors: {'email or password': 'is invalid'}});
                 }
                 return done(null , user);
             }).catch(done);
         }
-    )
+    ) 
 )
