70be471c213a87e2491974b137c32ebed4372011
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-07 17:32:06 +0000

    WIP (profiles)

--- models/User.js
+++ models/User.js
@@ -27,37 +27,47 @@ var UserSchema = new mongoose.Schema({
     salt: String
 }, { timestamps: true })
 
-UserSchema.plugin(uniqueValidator , {message : 'is already taken.'});
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
-UserSchema.methods.setPassword = function(password) {
+UserSchema.methods.setPassword = function (password) {
     this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt , 10000 , 512 , 'sha512').toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 }
 
-UserSchema.methods.validPassword = function( password) {
-    var hash = crypto.pbkdf2Sync(password , this.salt , 10000 , 512 , 'sha512').toString('hex');
-    return this.hash == hash ; 
+UserSchema.methods.validPassword = function (password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash == hash;
 }
 
-UserSchema.methods.generateJWT = function() {
+UserSchema.methods.generateJWT = function () {
     var today = new Date();
     var exp = new Date(today);
     exp.setDate(today.getDate() + 60);
 
     return jwt.sign({
-        id : this._id,
-        username : this.username,
-        exp : parseInt(exp.getTime()/1000)
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000)
     }, secret)
 }
 
-UserSchema.methods.toAuthJSON = function() {
+UserSchema.methods.toAuthJSON = function () {
     return {
-        username : this.username, 
-        email : this.email, 
-        token : this.generateJWT(),
-        bio : this.bio,
-        image : this.image
+        username: this.username,
+        email: this.email,
+        token: this.generateJWT(),
+        bio: this.bio,
+        image: this.image
     };
 }
+
+UserSchema.methods.toProfileJSONFor = function (user) {
+    return {
+        username: this.username,
+        bio: this.bio,
+        image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: false
+    };
+}
+
 mongoose.model('User', UserSchema);
