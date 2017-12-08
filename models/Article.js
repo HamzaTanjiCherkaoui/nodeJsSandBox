7ea4c681e1ec238cfd0ae306d5ce6e8a1836ebe4
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-08 11:28:54 +0000

    aadding Articles Schema

--- models/Article.js
+++ models/Article.js
@@ -2,3 +2,25 @@ var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var slug = require('slug');
 
+var ArticleSchema = mongoose.Schema({
+    slug: {
+        type: String,
+        lowercase: true,
+        unique: true
+    },
+    title: String,
+    description: String,
+    body: String,
+    favoritesCount: {
+        type: Number,
+        default: 0
+    },
+    tagList: [{ type: String }],
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+}, {timestamps: true});
+
+
