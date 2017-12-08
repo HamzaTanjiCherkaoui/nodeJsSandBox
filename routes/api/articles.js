3e4407c7f62172fb6603b3e16f4f2c8664765939
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-08 12:12:52 +0000

    adding get and post a single article

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -11,3 +11,23 @@ router.param('article', function (req, res, next, slug) {
         next();
     }).catch(next);
 })
+
+router.get('/:article', auth.required, function (req, res, next) {
+    Promise.all([
+        req.payload ? User.findById(req.payload.id) : null,
+        req.article.populate('author').execPopulate()
+    ]).then(function (results) {
+        var user = results[0];
+        return res.json({ article: req.article.toJSONFor(user) });
+    }).catch(next);
+})
+
+router.post('/', auth.required, function (req, res, next) {
+    User.findById(req.payload.id).then(function (user) {
+        if (!user) return res.sendStatus(401);
+        var article = new Article(req.body.article);
+        return article.save().then(function () {
+            return res.json({ article: article.toJSONFor(user) });
+        }).catch(next);
+    })
+})
