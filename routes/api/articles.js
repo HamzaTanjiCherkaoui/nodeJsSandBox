cb5a1002abf74cb95d645c1b0ded5150560494d9
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-08 12:00:51 +0000

    adding article middleware with router.param

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -4,3 +4,10 @@ var User = mongoose.model('User');
 var Article = mongoose.model('Article');
 var auth = require('../auth');
 
+router.param('article', function (req, res, next, slug) {
+    Article.findOne({ slug: slug }).then(function (article) {
+        if (!article) return res.sendStatus(404);
+        req.article = article;
+        next();
+    }).catch(next);
+})
