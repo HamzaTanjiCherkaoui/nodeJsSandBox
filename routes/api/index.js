e0189282e5d7ac2c908d864ba30b72c21f688f03
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-08 10:58:45 +0000

    adding profiles routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
 var router = require('express').Router();
 router.use('/', require('./users')); 
+router.use('/profiles', require('./profiles'));
 module.exports = router;
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -3,7 +3,8 @@ var router = require('express').Router();
 var User = mongoose.model('User');
 var auth = require('../auth');
 
-router.params('username',function(req,res,next,username){
+router.param('username',function(req,res,next,username){
+    console.log("passing username "+username);
     User.findOne({username : username}).then(function(user){
         if(!user) return res.sendStatus(404);
         req.profile = user;
@@ -12,5 +13,18 @@ router.params('username',function(req,res,next,username){
 })
 
 router.get('/:username' ,auth.optional,function(req,res,next){
-    
+
+    if(req.payload) {
+        
+        User.findOne(req.payload.id).then(function(user){
+            if(!user) return res.json({profile : req.profile.toProfileJSONFor(false)});
+            return res.json({profile : req.profile.toProfileJSONFor(user)});
+        }).catch(next);
+        
+    } else {
+        return res.json({profile : req.profile.toProfileJSONFor(flase)});
+    }
+
 })
+
+module.exports = router; 
