89ff9982b8c80d0d2832cd8348782b0945d15b58
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-09 14:09:01 +0000

    fixing unkonwn model issue

--- models/Article.js
+++ models/Article.js
@@ -2,44 +2,40 @@ var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var slug = require('slug');
 
-var ArticleSchema = mongoose.Schema({
-    slug: {
-        type: String,
-        lowercase: true,
-        unique: true
-    },
-    title: String,
-    description: String,
-    body: String,
-    favoritesCount: {
-        type: Number,
-        default: 0
-    },
-    tagList: [{ type: String }],
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+var ArticleSchema = new mongoose.Schema({
+  slug: {type: String, lowercase: true, unique: true},
+  title: String,
+  description: String,
+  body: String,
+  favoritesCount: {type: Number, default: 0},
+  tagList: [{ type: String }],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, {timestamps: true});
 
-}, { timestamps: true });
+ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
 
-mongoose.plugin(uniqueValidator, { message: 'is Already Taken' });
+ArticleSchema.pre('validate', function(next){
+  this.slugify();
 
-ArticleSchema.pre('validate', function (next) {
-    this.slugify();
-    next();
+  next();
 });
 
-ArticleSchema.methods.toJSONFor = function (user) { 
-    return {
-        slug: this.slug,
-        title: this.title,
-        description: this.description,
-        body: this.body,
-        tagList: this.tagList,
-        favoritesCount: this.favoritesCount,
-        createdAt: this.createdAt,
-        updatedAt: this.updatedAt,
-        author: this.author.toProfileJSONFor(user),  
-    };
-}
+ArticleSchema.methods.slugify = function() {
+  this.slug = slug(this.title);
+};
+
+ArticleSchema.methods.toJSONFor = function(user){
+  return { 
+    slug: this.slug,
+    title: this.title,
+    description: this.description,
+    body: this.body,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+    tagList: this.tagList,
+    favoritesCount: this.favoritesCount,
+    author: this.author.toProfileJSONFor(user)
+  };
+};
+
+mongoose.model('Article', ArticleSchema);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
 router.use('/', require('./users')); 
 router.use('/profiles', require('./profiles'));
+router.use('/articles', require('./articles'));
 module.exports = router;
