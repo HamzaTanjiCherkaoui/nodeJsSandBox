3b46f171d7cb35b61d3ff29e147df76deb61a399
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-10 02:00:21 +0000

    adding unfavoriting articles

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
 var User = mongoose.model('User');
+var Comment = mongoose.model('Comment');
 var auth = require('../auth');
 
 router.param('article', function (req, res, next, slug) {
@@ -45,5 +46,20 @@ router.post('/:article/favorite', auth.required, function (req, res, next) {
 
         })
     }).catch(next);
+})
+router.delete('/:article/favorite', auth.required, function (req, res, next) {
+    var articleId = req.article._id;
+    User.findById(req.payload.id).then(function (user) {
+        if (!user) return res.sendStatus(401);
+        return user.unFavorite(articleId).then(function () {
+            return req.article.updateFavoritesCount().then(function (article) {
+                return res.json({ article: article.toJSONFor(user) });
+            }).catch(next);
+
+        })
+    }).catch(next);
+})
+router.get('/:article/comments', auth.optional, function (req, res, next) {
+
 })
 module.exports = router;
