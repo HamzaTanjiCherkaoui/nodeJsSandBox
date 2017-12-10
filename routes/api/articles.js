8b70c91c7b5980206da8e9f465c0707be9f178ed
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-10 01:31:17 +0000

    adding favoriting routes and updating favorites count

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,18 +1,18 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
-var User = mongoose.model('User'); 
+var User = mongoose.model('User');
 var auth = require('../auth');
 
 router.param('article', function (req, res, next, slug) {
-    Article.findOne({ slug: slug }).then(function (article) {
+    Article.findOne({ slug: slug }).populate('author').then(function (article) {
         if (!article) return res.sendStatus(404);
         req.article = article;
         next();
     }).catch(next);
 })
 
-router.get('/:article', auth.required, function (req, res, next) {
+router.get('/:article', auth.optional, function (req, res, next) {
     Promise.all([
         req.payload ? User.findById(req.payload.id) : null,
         req.article.populate('author').execPopulate()
@@ -28,10 +28,22 @@ router.post('/', auth.required, function (req, res, next) {
         var article = new Article(req.body.article);
         article.author = user;
         return article.save().then(function () {
-            
+
             return res.json({ article: article.toJSONFor(user) });
         }).catch(next);
     })
 })
 
+router.post('/:article/favorite', auth.required, function (req, res, next) {
+    var articleId = req.article._id;
+    User.findById(req.payload.id).then(function (user) {
+        if (!user) return res.sendStatus(401);
+        return user.favorite(articleId).then(function () {
+            return req.article.updateFavoritesCount().then(function (article) {
+                return res.json({ article: article.toJSONFor(user) });
+            }).catch(next);
+
+        })
+    }).catch(next);
+})
 module.exports = router;
