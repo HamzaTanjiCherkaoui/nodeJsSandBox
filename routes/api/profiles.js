c35f7852660e957249e5b2625343742caf87600d
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-11 00:34:18 +0000

    adding unfollow route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -29,10 +29,21 @@ router.get('/:username', auth.optional, function (req, res, next) {
 router.post('/:username/follow', auth.required, function (req, res, next) {
     var profileId = req.profile._id;
     User.findById(req.payload.id).then(function (user) {
-        if (!user) res.sendStatus(403);
+        if (!user) res.sendStatus(401);
         return user.follow(profileId).then(function () {
             return res.json({ profile: req.profile.toProfileJSONFor(user) })
         }).catch(next);
+    })
+})
+
+router.delete('/:username/unfollow',auth.required,function(req,res,next){
+    var profileId = req.profile._id;
+    User.findById(req.payload.id).then(function(user){
+        if (!user) res.sendStatus(401);
+        return user.unfollow(profileId).then(function(){
+            return res.json({profile : req.profile.toProfileJSONFor(user)});
+        }).catch(next);
+
     })
 })
 
