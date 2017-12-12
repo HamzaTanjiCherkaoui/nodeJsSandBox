f3b28e6147e4e7c82571e997159482dcb5ef08a1
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-12 17:44:00 +0000

    adding get all articles

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -20,6 +20,57 @@ router.param('comment', function (req, res, next, id) {
     }).catch(next);
 })
 
+router.get('/', auth.optional, function (req, res, next) {
+    
+    var query = {};
+    var limit = 20;
+    var offset = 0;
+
+    if (typeof req.body.limit !== 'undefined') limit = req.body.limit;
+    if (typeof req.body.offset !== 'undefined') offset = req.body.offset;
+    if (typeof req.body.tag !== 'undefined') query.tagList = { "$in": [req.body.tag] };
+
+
+    Promise.all([
+        req.query.author ? User.findOne({ username: req.query.athor }) : null,
+        req.query.favorited ? User.findOne({ username: req.body.favorited }) : null
+    ]).then(function (results) {
+        var author = results[0];
+        var favoriter = results[1];
+        if (author) query.author = author._id;
+
+        if (favoriter) {
+            query._id = { "$in": favoriter.favorites };
+        } else if (req.query.favorited) {
+            query._id = { '$in': [] };
+        }
+
+        return Promise.all([
+            Article.find(query)
+                .limit(Number(limit))
+                .skip(Number(offset))
+                .sort({ createdAt: 'desc' })
+                .populate('author')
+                .exec(),
+            Article.count(query).exec(),
+            req.payload ? User.findById(req.payload.id) : null
+        ]).then(function (results) {
+            var articles = results[0];
+            var articlesCount = results[1];
+            var user = results[2];
+            
+            return res.json({
+                articles : articles.map(function(article){
+                    
+                    return article.toJSONFor(user);
+                }),
+                articlesCount : articlesCount
+            });
+            
+        }).catch(next);
+    }).catch(next);
+
+})
 router.get('/:article', auth.optional, function (req, res, next) {
     Promise.all([
         req.payload ? User.findById(req.payload.id) : null,
@@ -107,9 +158,9 @@ router.delete('/:article/comments/:comment', auth.required, function (req, res, 
         req.article.comments.remove(req.comment._id);
         req.article.save()
             .then(Comment.find({ _id: req.comment._id }).remove().exec())
-            .then(function () {  res.sendStatus(204) });
+            .then(function () { res.sendStatus(204) });
     } else {
-         res.sendStatus(403);
+        res.sendStatus(403);
     }
 })
 module.exports = router;
