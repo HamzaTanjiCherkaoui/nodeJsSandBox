1e557340160cf87fe67f4d64c6c38d0565bb3e7f
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-15 16:55:57 +0000

    adding verification token generation

--- models/Token.js
+++ models/Token.js
@@ -1,3 +1,17 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var crypto = require('crypto');
+
+var TokenSchema = mongoose.Schema({
+    _userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    token: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now, expires: 86400 }
+});
+
+UserSchema.methods.setToken = function (user) {
+    var salt = crypto.randomBytes(16).toString('hex');
+    this.token = crypto.pbkdf2Sync({_userId : user._id}, this.salt, 10000, 512, 'sha512').toString('hex');
+
+}
+
+mongoose.model('Token',TokenSchema);
